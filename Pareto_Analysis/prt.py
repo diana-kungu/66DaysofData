695efe13b4fb25f67f02d08e7a2eb304c331afe9
Diana Kung'u <dkungu@gmail.com>
2022-04-05 23:05:14 +0300

    Readme.md updated

--- Pareto_Analysis/prt.py
+++ Pareto_Analysis/prt.py
@@ -3,6 +3,7 @@
 from datetime import datetime
 from pandas import read_csv
 import streamlit as st
+from streamlit_option_menu import option_menu
 from datetime import datetime
 
 #Read Data
@@ -47,6 +48,18 @@ st.sidebar.markdown(
     """,
     unsafe_allow_html=True,
 )
+
+with st.sidebar:
+    choose = option_menu("App Gallery", ["About", "Photo Editing", "Project Planning", "Python e-Course", "Contact"],
+                         icons=['house', 'camera fill', 'kanban', 'book','person lines fill'],
+                         menu_icon="app-indicator", default_index=0,
+                         styles={
+        "container": {"padding": "5!important", "background-color": "#fafafa"},
+        "icon": {"color": "orange", "font-size": "25px"}, 
+        "nav-link": {"font-size": "16px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
+        "nav-link-selected": {"background-color": "#02ab21"},
+    }
+    )
 
 
 #Pareto body
